807dcfa62fd7e3da7b80c919f77c0909a6eaaa83
Eder <eder@joker.(none)>
2012-11-22 19:48:09 -0200

    Fix typo mistake

--- oflib/oxm-match.c
+++ oflib/oxm-match.c
@@ -749,7 +749,7 @@ int oxm_put_match(struct ofpbuf *buf, struct ofl_match *omt){
                          uint32_t mask;
                          memcpy(&mask,oft->value + length ,sizeof(uint32_t));
 						 if (oft->header == OXM_OF_IPV4_DST_W|| oft->header == OXM_OF_IPV4_SRC_W
-							||oft->header == OXM_OF_ARP_SPA_W || oft->header == OXM_OF_ARP_TPA)_W
+							||oft->header == OXM_OF_ARP_SPA_W || oft->header == OXM_OF_ARP_TPA_W)
                             oxm_put_32w(buf, oft->header, value, mask); 
 						 else
 							oxm_put_32w(buf, oft->header, htonl(value),htonl(mask));
