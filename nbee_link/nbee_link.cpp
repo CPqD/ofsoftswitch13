684a76c707ee211209d1798a0731b12724585099
cloudysunny14 <lakshmi@cloudysunny14.org>
2014-01-27 20:47:37 +0900

    Match field MPLS TC and MPLS BOS doesn't work

--- nbee_link/nbee_link.cpp
+++ nbee_link/nbee_link.cpp
@@ -265,14 +265,14 @@ int nblink_extract_proto_fields(struct ofpbuf * pktin, _nbPDMLField * field, str
             ofl_structs_match_put32(pktout, header, m_value);
         }
         else if (header == OXM_OF_MPLS_TC){
-            uint8_t m_value;
-            sscanf(field->Value, "%hhx", &m_value);
+            uint32_t m_value;
+            sscanf(field->Value, "%x", &m_value);
             m_value = (m_value & MPLS_TC_MASK) >> MPLS_TC_SHIFT;
-            ofl_structs_match_put32(pktout, header, m_value);
+            ofl_structs_match_put8(pktout, header, m_value);
         }
         else if (header == OXM_OF_MPLS_BOS){
-            uint8_t m_value;
-            sscanf(field->Value, "%hhx", &m_value);
+            uint32_t m_value;
+            sscanf(field->Value, "%x", &m_value);
             m_value = (m_value & MPLS_S_MASK) >> MPLS_S_SHIFT;
             ofl_structs_match_put8(pktout, header, m_value);
         }
