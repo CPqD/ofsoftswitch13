bd8e7b68951c5ef2293d0a0e45c89b265a2eba17
Jean Tourrilhes <jean.tourrilhes@hp.com>
2014-08-25 16:50:09 -0700

    Finish reverting EXT-192/EXT-276 Role Status (1.4 feature).

--- oflib/ofl-messages.c
+++ oflib/ofl-messages.c
@@ -277,8 +277,7 @@ ofl_msg_free(struct ofl_msg_header *msg, struct ofl_exp *exp) {
             break;
         }
         case OFPT_ROLE_REPLY:
-        case OFPT_ROLE_REQUEST:
-        case OFPT_ROLE_STATUS:{
+        case OFPT_ROLE_REQUEST:{
             break;
         }
         case OFPT_GET_ASYNC_REPLY:
