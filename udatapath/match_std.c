355e0fdc7366218351e4e364b9882618512d22e9
Eder Leão Fernandes <ederleaofernandes@gmail.com>
2014-12-17 20:41:33 -0200

    Fix match_mask32.
    This commit fixes the cast of the mask for matching
    masked 32 bits fields.

--- udatapath/match_std.c
+++ udatapath/match_std.c
@@ -95,9 +95,8 @@ static inline bool
 match_mask32(uint8_t *a, uint8_t *am, uint8_t *b) {
     uint32_t *a1 = (uint32_t *) a;
     uint32_t *b1 = (uint32_t *) b;
-    uint32_t mask = (uint32_t) ((uint8_t) ~am[0] << 24 | (uint8_t) ~am[1] << 16 
-        | (uint8_t) ~am[2] << 8 | (uint8_t) ~am[3]);  
-    return (((mask) & (*a1 ^ *b1)) == 0);
+    uint32_t *mask = (uint32_t*) am;
+    return (((*mask) & (*a1 ^ *b1)) == 0);
 }
 
 /* Returns true if two 48 bit values match */
