94f6488dfd36255fa8b9e9722f2c6cb338d5cc5e
Eder Leão Fernandes <ederleaofernandes@gmail.com>
2016-07-15 18:26:03 +0100

    Enable packet string representation
    This commit reintroduces a commented function
    that prints the representation of the struct
    packet.

--- udatapath/packet.c
+++ udatapath/packet.c
@@ -133,7 +133,7 @@ packet_to_string(struct packet *pkt) {
     fprintf(stream, "\", buffer=\"");
     ofl_buffer_print(stream, pkt->buffer_id);
     fprintf(stream, "\", std=");
-    //packet_handle_std_print(stream, pkt->handle_std);
+    packet_handle_std_print(stream, pkt->handle_std);
     fprintf(stream, "}");
 
     fclose(stream);
